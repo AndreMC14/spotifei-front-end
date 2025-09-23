8df66bc544137d7c6c874771110b8b3e8937dd6e
hard87 <juniorgodoi1987@gmail.com>
2025-09-23 18:03:56 -0300

    Envio tarefa03 Página de busca e relacionados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-search-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
