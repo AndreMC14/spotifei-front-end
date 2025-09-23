a7a3681ca5c8d34e0e7cf2e1ca7d239e0b0ba851
Andremc1402 <andretangerina03@gmail.com>
2025-09-22 21:46:23 -0300

    Implementação do componente cadastro 100%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CadastroComponent],  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Spotifei';
-}
+export class AppComponent { }
