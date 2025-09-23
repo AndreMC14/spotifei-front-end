feeca057b39985de3a37c95fbc7420211076611a
Marcos-Vinicius <marcosvinicios10000p@gmail.com>
2025-09-22 21:11:57 -0300

    Criacao do componente minha-biblioteca

---
+++ src/app/components/minha-biblioteca/minha-biblioteca.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minha-biblioteca',
+  standalone: true,
+  imports: [],
+  templateUrl: './minha-biblioteca.component.html',
+  styleUrl: './minha-biblioteca.component.css'
+})
+export class MinhaBibliotecaComponent {
+
+}
