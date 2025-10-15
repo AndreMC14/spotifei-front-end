b932d6d4e0f0e4ff6d2eef774f65407f00a6c0d8
Marcossou13 <marcosvinicios10000p@gmail.com>
2025-10-14 21:54:33 -0300

    Atualizacao no desingn e criacao dos cards-bibliotecas

---
+++ src/app/components/minha-biblioteca/biblioteca-card/biblioteca-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biblioteca-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './biblioteca-card.component.html',
+  styleUrl: './biblioteca-card.component.css'
+})
+export class BibliotecaCardComponent {
+
+}
--- src/app/components/minha-biblioteca/minha-biblioteca.component.ts
+++ src/app/components/minha-biblioteca/minha-biblioteca.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BibliotecaCardComponent } from '../minha-biblioteca/biblioteca-card/biblioteca-card.component'
 
 @Component({
   selector: 'app-minha-biblioteca',
   standalone: true,
-  imports: [],
+  imports: [BibliotecaCardComponent],
   templateUrl: './minha-biblioteca.component.html',
   styleUrl: './minha-biblioteca.component.css'
 })
